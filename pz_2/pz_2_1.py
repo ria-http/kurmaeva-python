b773f83ce9a1f5f6661ab6d4e5709a00e489c95c
ria-http <kurmaevadara7@gmail.com>
2024-10-24 00:18:34 +0300

    добавлена полная программа

--- pz_2/pz_2_1.py
+++ pz_2/pz_2_1.py
@@ -1,1 +1,19 @@
-print('axxaxaxaxa!!!')
+#Вариант 16. Дано трехзначное число. Найти сумму и произведение его цифр.
+num = input('введите 3-значное число ')
+while type(num) != int:   #обработка исключений
+  try:
+    num = int(num)
+    if 99 < num < 1000:   #проверка на 3-х значное число
+      sot = num // 100
+      des = (num // 10) % 10
+      ed = num % 10
+      print(sot, des, ed)
+      summ = sot + des + ed
+      pr = sot * des * ed
+      print(' сумма чисел', summ, '\n', 'произведение чисел', pr)
+    else:
+      print('число не 3-значное')
+      num = input('введите 3-значное число ')
+  except ValueError:
+    print('вы ввели не число')
+    num = input('введите 3-значное число ')
