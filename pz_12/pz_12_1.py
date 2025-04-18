4bade08bbfea84d405a9f4bb77aa621743411e2a
ria-http <kurmaevadara7@gmail.com>
2025-04-18 15:28:14 +0300

    fix

--- pz_12/pz_12_1.py
+++ pz_12/pz_12_1.py
@@ -1,12 +1,7 @@
 # В последовательности на n целых чисел умножить все элементы на первый элемент
 import random
 n = int(input('введите количество чисел '))
-
-
 listt = ([random.randint(0,50) for el in range(n)])
-for el in range(n):
-  el = random.randint(0,50)
-  listt.append(el)
 
 first = listt[0]
 new = [i * first for i in listt]
