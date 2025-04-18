a5ac4c2ba371c1cde4a6ea3eef11838e093b2de0
ria-http <kurmaevadara7@gmail.com>
2025-04-18 18:32:22 +0300

    fix

--- pz_12/tte.py
+++ pz_12/tte.py
@@ -1,11 +1,11 @@
 import random
 
-# print([i ** 3 for i in range(0,15) if i % 2 == 0])
+# print([i ** 3 for i in range(0,15) if i % 2 == 0])  # возвести в куб четные числа
 
-# names = input("hdjdj ")
+# names = input("hdjdj ")  # писать имена с большой буквы  в списке
 # spl = list(names.split(' '))
 # print([el.title() for el in spl])
 
-word = input('ВВЕДИТЕ СЛОВА ')
+word = input('ВВЕДИТЕ СЛОВА ')  # добавить слова в список, если их длина больше 4
 listt = list(word.split(' '))
 print([el for el in listt if len(el) > 4])
